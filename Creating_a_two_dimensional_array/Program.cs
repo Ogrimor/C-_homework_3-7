d1f3e9207689e09c522cd1db490836ab87706ba2
Ogrimor <ogrimors37@gmail.com>
2022-11-10 15:24:57 +0500

    Creating a two dimensional array

---
+++ Creating_a_two_dimensional_array/Program.cs
@@ -0,0 +1,37 @@
+﻿int GetValue (string msg)
+{
+    Console.WriteLine(msg);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+double[,] GetArray(int a, int b)
+{
+    double[,] array = new double[a,b];
+    Random rand = new Random();
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            array[i,j] = Math.Round(rand.NextDouble(), 2);
+        }
+    }
+    return array;
+}
+
+void printArr(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int arrayСolumns = GetValue("Введите количество столбцов массива: ");
+int arrayStrings = GetValue("Введите количество строк массива: ");
+double[,] array  = GetArray(arrayСolumns, arrayStrings);
+printArr(array);
