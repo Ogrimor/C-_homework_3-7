5161fca0ba1b850ddcd775a2c5a4bab196d22ecb
Ogrimor <ogrimors37@gmail.com>
2022-11-10 17:45:48 +0500

    3-6

---
+++ Count_of_positive_numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Сколько чисел Вы хотите ввести? - ");
+int arreyNumbersLength = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[arreyNumbersLength];
+int count = 0;
+
+for(int i = 0; i < arreyNumbersLength; i++)
+{
+    Console.Write($"A[{i + 1}] = ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < arreyNumbersLength; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Количество введённых положительных чисел: " + count);
---
+++ Point_of_intersection_of_lines/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите значение b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1)/(k1 - k2);
+double y1 = k1 * x + b1;
+double y2 = k2 * x + b2;
+Console.WriteLine("(" + y1 + "; " + y2 + ")");
+
